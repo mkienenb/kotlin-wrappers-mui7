adf80965644bf154fc179a310fd625e119f6dfbd
Mike Kienenberger <mkienenb@gmail.com>
2026-04-28 13:19:18 -0400

    Move wrappersVersion back into settings.gradle.kts and update to "2026.4.12"

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,7 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("kotlinWrappers") {
-            val wrappersVersion = extra["kotlin-wrappers.version"] as String
+            val wrappersVersion = "2026.4.12"
             from("org.jetbrains.kotlin-wrappers:kotlin-wrappers-catalog:$wrappersVersion")
         }
     }
